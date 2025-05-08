d66bf6b29f67f804c65b4e5d6f3bc59c217d94fd
Rishi Sivaraju <sivarajurishi@gmail.com>
2025-05-08 23:07:16 +0530

    cors done2

--- server.js
+++ server.js
@@ -31,26 +31,26 @@ const allowedOrigins = [
 app.use(cors());
 
 // Custom handling of headers
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
 
-  // If origin matches, allow it
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Moved inside origin match
-  }
+//   // If origin matches, allow it
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Moved inside origin match
+//   }
 
-  // Always set method and headers
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   // Always set method and headers
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-  // Handle preflight OPTIONS requests
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204); // No Content
-  }
+//   // Handle preflight OPTIONS requests
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(204); // No Content
+//   }
 
-  next();
-});
+//   next();
+// });
 
 
 
