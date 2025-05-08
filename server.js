b2d4723cd21c6ff7851eaa7ef4774d0ee8205648
Rishi Sivaraju <sivarajurishi@gmail.com>
2025-05-08 23:00:24 +0530

    cors done

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   // Allow credentials
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   // Handle preflight OPTIONS requests
   if (req.method === 'OPTIONS') {
