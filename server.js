763153733c45008ec04b0fa9cbf8e8a93fd7bba3
Rishi Sivaraju <sivarajurishi@gmail.com>
2025-05-08 12:01:07 +0530

    CORS ERROE IS ADDRESSE6

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ const app = express();
 
 // ✅ Apply globally before any routes
 app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 // app.options('*', cors(corsOptions)); 
 
 // const corsOptions = {
